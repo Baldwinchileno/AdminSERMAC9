3906d3e2a1c6e95954cc3a92e5327fb567cb9274
Baldwinchileno <140751612+Baldwinchileno@users.noreply.github.com>
2025-01-07 11:28:32 -0300

    Add files via upload

---
+++ Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace AdminSERMAC.Models
+{
+    public class Cliente
+    {
+        public string? RUT { get; set; }
+        public string? Nombre { get; set; }
+        public string? Direccion { get; set; }
+        public string? Giro { get; set; }
+        public double Deuda { get; set; }
+    }
+}
